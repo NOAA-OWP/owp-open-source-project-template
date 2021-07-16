d8973a5622510b509478ba5bc0bfdcdc607f4e8c
Jason Regina <jarq6c@gmail.com>
2021-07-16 13:34:19 -0500

    add simple hello package

---
+++ python/miniconda/src/hello/hello.py
@@ -0,0 +1,17 @@
+def greet(name: str):
+    """
+    Create a simple greeting.
+
+    Parameters
+    ----------
+    name: str, required
+        Name of person to greet.
+
+    Returns
+    -------
+    greeting: str
+        String with a simple greeting.
+    
+    """
+    # Return greeting
+    return f"Hello, {name}!"
